5e99a0bdbf0017ad2ca87cfa069408cf9323d32d
hebataha <amltaha866@gmail.com>
2023-09-24 16:53:20 +0300

    dishes

--- src/apps/front-office/home/pages/HomePage/PopularDishes/PopularDishes.tsx
+++ src/apps/front-office/home/pages/HomePage/PopularDishes/PopularDishes.tsx
@@ -1,15 +1,15 @@
 import PopularDishesItem from "./PopularDishesItem/PopularDishesItem";
 import PopularDishesTabs from "./PopularDishesTabs/PopularDishesTabs";
-
+import styles from "./styles.module.scss"
 export type PopularDishesProps = {
   children: React.ReactNode;
 };
 export default function PopularDishes() {
   return (
-    <>
+    <div className={styles.popularDishes}>
       <h1>PopularDishes</h1>
       <PopularDishesTabs />
       <PopularDishesItem />
-    </>
+    </div>
   );
 }
--- src/apps/front-office/home/pages/HomePage/PopularDishes/PopularDishesItem/PopularDishesItem.tsx
+++ src/apps/front-office/home/pages/HomePage/PopularDishes/PopularDishesItem/PopularDishesItem.tsx
@@ -1,10 +1,122 @@
+import { IconHeart } from "@tabler/icons-react";
+import Pizze from "/public/images/menuItems/pizza.png";
+import Container from "@material-ui/core/Container";
+import styles from "./styles.module.scss";
+import { Card } from "@material-ui/core";
 export type PopularDishesItemProps = {
   children: React.ReactNode;
 };
 export default function PopularDishesItem() {
   return (
     <>
-      <h1>PopularDishesItem</h1>
+      <Container className={styles.cardContainer}>
+        <div className={styles.PopularDishesItemWrapper}>
+          <Card variant="outlined" className={styles.PopularDishesItem}>
+            <Container>
+              <div className={styles.heartIcon}>
+                <IconHeart color="red" fill="red" size={30}></IconHeart>
+              </div>
+              <div className={styles.imageWrapper}>
+                <div className={styles.image}>
+                  <img src={Pizze} />
+                </div>
+              </div>
+              <div className={styles.info}>
+                <h3>Italiano Original</h3>
+                <p>
+                  Ricotta, sun dried tomatoes, garlic, mozzarella cheese, topped
+                  with lightly
+                </p>
+                <span className={styles.itemPrice}>£12.63</span>
+              </div>
+            </Container>
+          </Card>
+          {/*  */}
+          <Card variant="outlined" className={styles.PopularDishesItem}>
+            <Container>
+              <div className={styles.heartIcon}>
+                <IconHeart color="red" fill="red" size={30}></IconHeart>
+              </div>
+              <div className={styles.imageWrapper}>
+                <div className={styles.image}>
+                  <img src={Pizze} />
+                </div>
+              </div>
+              <div className={styles.info}>
+                <h3>Italiano Original</h3>
+                <p>
+                  Ricotta, sun dried tomatoes, garlic, mozzarella cheese, topped
+                  with lightly
+                </p>
+                <span className={styles.itemPrice}>£12.63</span>
+              </div>
+            </Container>
+          </Card>
+             {/*  */}
+             <Card variant="outlined" className={styles.PopularDishesItem}>
+            <Container>
+              <div className={styles.heartIcon}>
+                <IconHeart color="red" fill="red" size={30}></IconHeart>
+              </div>
+              <div className={styles.imageWrapper}>
+                <div className={styles.image}>
+                  <img src={Pizze} />
+                </div>
+              </div>
+              <div className={styles.info}>
+                <h3>Italiano Original</h3>
+                <p>
+                  Ricotta, sun dried tomatoes, garlic, mozzarella cheese, topped
+                  with lightly
+                </p>
+                <span className={styles.itemPrice}>£12.63</span>
+              </div>
+            </Container>
+          </Card>
+             {/*  */}
+             <Card variant="outlined" className={styles.PopularDishesItem}>
+            <Container>
+              <div className={styles.heartIcon}>
+                <IconHeart color="red" fill="red" size={30}></IconHeart>
+              </div>
+              <div className={styles.imageWrapper}>
+                <div className={styles.image}>
+                  <img src={Pizze} />
+                </div>
+              </div>
+              <div className={styles.info}>
+                <h3>Italiano Original</h3>
+                <p>
+                  Ricotta, sun dried tomatoes, garlic, mozzarella cheese, topped
+                  with lightly
+                </p>
+                <span className={styles.itemPrice}>£12.63</span>
+              </div>
+            </Container>
+          </Card>
+             {/*  */}
+             <Card variant="outlined" className={styles.PopularDishesItem}>
+            <Container>
+              <div className={styles.heartIcon}>
+                <IconHeart color="red" fill="red" size={30}></IconHeart>
+              </div>
+              <div className={styles.imageWrapper}>
+                <div className={styles.image}>
+                  <img src={Pizze} />
+                </div>
+              </div>
+              <div className={styles.info}>
+                <h3>Italiano Original</h3>
+                <p>
+                  Ricotta, sun dried tomatoes, garlic, mozzarella cheese, topped
+                  with lightly
+                </p>
+                <span className={styles.itemPrice}>£12.63</span>
+              </div>
+            </Container>
+          </Card>
+        </div>
+      </Container>
     </>
   );
 }

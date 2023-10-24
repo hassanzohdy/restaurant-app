b3bf16be0126bbec6817f63e97da1a492cc53d24
hassanzohdy <hassanzohdy@gmail.com>
2023-10-24 19:17:17 +0300

    Created cart module

---
+++ src/apps/front-office/cart/pages/CartPage/CartPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function _CartPage() {
+  return (
+    <>
+      <h1>CartPage</h1>
+    </>
+  );
+}
+
+const CartPage = React.memo(_CartPage);
+export default CartPage;

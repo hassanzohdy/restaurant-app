b968ec45a91467a9d3c9afc2f468797d71a7ac8d
omar1.taha <omar.taha@macpractice.com>
2023-11-29 14:14:04 +0200

    link categories in sidebar to api

--- src/apps/front-office/menu/components/MenuSidebarSidebar.tsx
+++ src/apps/front-office/menu/components/MenuSidebarSidebar.tsx
@@ -1,59 +1,27 @@
 import { CiBowlNoodles, CiBurger, CiCoffeeCup, CiPizza } from "react-icons/ci";
+import { TbCake } from "react-icons/tb";
+
 import { BestDealItemList } from "./BestDealItem";
 import "./MenuSidebarSidebar.scss";
 
-function MenuSidebar() {
+type MenuSidebarProps = {
+  categories: any;
+  onCategorySelect: (cat: string) => void;
+};
+
+export default function MenuSidebar(props: MenuSidebarProps) {
+  const { categories } = props;
+
   return (
     <div className="flex flex-col sidebar">
       <div className="h-full  border-2 rounded-2xl">
         <h2 className="font-black my-4 ml-4 text-lg">Categories</h2>
-        <ul role="list" className="m-4 p-4 categoryList rounded-2xl">
-          <li className="flex justify-between mb-3">
-            <div className="flex flex-row">
-              <CiBurger className="text-2xl mr-2" />
-              <a href="#">Burgers</a>
-            </div>
-            <div>
-              <p>(0)</p>
-            </div>
-          </li>
-          <li className="flex justify-between my-3">
-            <div className="flex flex-row">
-              <CiCoffeeCup className="text-2xl mr-2" />
-              <a href="#">Cold Drinkes</a>
-            </div>
-            <div>
-              <p>(0)</p>
-            </div>
-          </li>
-          <li className="flex justify-between my-3">
-            <div className="flex flex-row">
-              <CiCoffeeCup className="text-2xl mr-2" />
-              <a href="#">Hot Drinkes</a>
-            </div>
-            <div>
-              <p>(0)</p>
-            </div>
-          </li>
-          <li className="flex justify-between my-3">
-            <div className="flex flex-row">
-              <CiBowlNoodles className="text-2xl mr-2" />
-              <a href="#">Pasta</a>
-            </div>
-            <div>
-              <p>(0)</p>
-            </div>
-          </li>
-          <li className="flex justify-between">
-            <div className="flex flex-row">
-              <CiPizza className="text-2xl mr-2" />
-              <a href="#">Pizza</a>
-            </div>
-            <div>
-              <p>(0)</p>
-            </div>
-          </li>
-        </ul>
+        {/* Category list component takes categories object (key-CategoryName: value-CategoryCount) from parent component
+        and loop on it to create category list */}
+        <CateogoryList
+          categories={categories}
+          onCategorySelect={props.onCategorySelect}
+        />
       </div>
       <div>
         <input
@@ -67,4 +35,48 @@ function MenuSidebar() {
     </div>
   );
 }
-export default MenuSidebar;
+
+export function CateogoryList(props: MenuSidebarProps) {
+  const { categories } = props;
+  const keysArr = Object.keys(categories);
+
+  return (
+    <ul role="list" className="m-4 p-4 categoryList rounded-2xl">
+      {keysArr.map((category, index) => {
+        return (
+          <li key={index} className="mb-3">
+            <button
+              className="flex justify-between"
+              onClick={() => props.onCategorySelect(category)}>
+              <div className="flex flex-row">
+                <CategoryImage categoryName={category} />
+                <p>{category}</p>
+              </div>
+              <div>
+                <p>({categories[category] ?? 0})</p>
+              </div>
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export function CategoryImage(props: { categoryName: string }) {
+  const iconClassName = "text-2xl mr-2";
+  switch (props.categoryName) {
+    case "Burgers":
+      return <CiBurger className={iconClassName} />;
+    case "Cold Drinks":
+      return <CiCoffeeCup className={iconClassName} />;
+    case "Hot Drinks":
+      return <CiCoffeeCup className={iconClassName} />;
+    case "Pasta":
+      return <CiBowlNoodles className={iconClassName} />;
+    case "Pizza":
+      return <CiPizza className={iconClassName} />;
+    case "Cake":
+      return <TbCake className={iconClassName} />;
+  }
+}
